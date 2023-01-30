fcf8114ddd8ad02519d32a0a1298e4c2a96cb5cb
Bad_S <Evlampyev.Alex@gmail.com>
2023-01-30 20:53:25 +0500

    default command

--- Lesson IX/bot.py
+++ Lesson IX/bot.py
@@ -1,6 +1,6 @@
 # token1 = "5916882745:AAHDomrnWWXirqjByn6-XiztHz9nDXolK1Q"
 from telegram import Bot
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from random import randint
 
 token1 = "5916882745:AAHDomrnWWXirqjByn6-XiztHz9nDXolK1Q"
@@ -17,12 +17,19 @@ def start(update, contex):
 def rand(update, contex):
     contex.bot.send_message(update.effective_chat.id, f'{randint(1, 100)}')
 
+
+def default(updater, contex):
+    contex.bot.send_message(updater.effective_chat.id, "Я не знаю таких команд")
+
+
 print("Server start")
 start_handler = CommandHandler('start', start)
 random_handler = CommandHandler('random', rand)
+default_handler = MessageHandler(Filters.command, default)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(random_handler)
+dispatcher.add_handler(default_handler)
 
 updater.start_polling()
 updater.idle()
